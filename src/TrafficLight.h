b01d7ad57c34920394127bcd2c922e64fb092980
Michael Henrique <oliveirasantos.mh@gmail.com>
2020-04-18 16:52:30 -0300

    Commented added

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -12,7 +12,7 @@ enum TrafficLightPhase { red, green };
 // forward declarations to avoid include cycle
 class Vehicle;
 
-// FP.3 Define a class "MessageQueue“ which has the public methods send and
+// FP.3 Define a class "MessageQueue" which has the public methods send and
 // receive. Send should take an rvalue reference of type TrafficLightPhase
 // whereas receive should return this type. Also, the class should define an
 // std::dequeue called _queue, which stores objects of type TrafficLightPhase.
@@ -25,12 +25,12 @@ class MessageQueue {
  private:
 };
 
-// FP.1 : Define a class "TrafficLight“ which is a child class of TrafficObject.
-// The class shall have the public methods "void waitForGreen()“ and "void
-// simulate()“ as well as "TrafficLightPhase getCurrentPhase()“, where
-// TrafficLightPhase is an enum that can be either "red“ or "green“. Also, add
-// the private method "void cycleThroughPhases()“. Furthermore, there shall be
-// the private member _currentPhase which can take "red“ or "green“ as its
+// FP.1 : Define a class "TrafficLight" which is a child class of TrafficObject.
+// The class shall have the public methods "void waitForGreen()" and "void
+// simulate()" as well as "TrafficLightPhase getCurrentPhase()", where
+// TrafficLightPhase is an enum that can be either "red" or "green". Also, add
+// the private method "void cycleThroughPhases()". Furthermore, there shall be
+// the private member _currentPhase which can take "red" or "green" as its
 // value.
 
 class TrafficLight : public TrafficObject {
