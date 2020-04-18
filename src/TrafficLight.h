f6a7350c6da4f9600afba6de268b71f15ca877fd
Michael Henrique <oliveirasantos.mh@gmail.com>
2020-04-18 16:49:13 -0300

    Task 1 Completed

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -7,10 +7,12 @@
 
 #include "TrafficObject.h"
 
+enum TrafficLightPhase { red, green };
+
 // forward declarations to avoid include cycle
 class Vehicle;
 
-// FP.3 Define a class „MessageQueue“ which has the public methods send and
+// FP.3 Define a class "MessageQueue“ which has the public methods send and
 // receive. Send should take an rvalue reference of type TrafficLightPhase
 // whereas receive should return this type. Also, the class should define an
 // std::dequeue called _queue, which stores objects of type TrafficLightPhase.
@@ -23,21 +25,27 @@ class MessageQueue {
  private:
 };
 
-// FP.1 : Define a class „TrafficLight“ which is a child class of TrafficObject.
-// The class shall have the public methods „void waitForGreen()“ and „void
-// simulate()“ as well as „TrafficLightPhase getCurrentPhase()“, where
-// TrafficLightPhase is an enum that can be either „red“ or „green“. Also, add
-// the private method „void cycleThroughPhases()“. Furthermore, there shall be
-// the private member _currentPhase which can take „red“ or „green“ as its
+// FP.1 : Define a class "TrafficLight“ which is a child class of TrafficObject.
+// The class shall have the public methods "void waitForGreen()“ and "void
+// simulate()“ as well as "TrafficLightPhase getCurrentPhase()“, where
+// TrafficLightPhase is an enum that can be either "red“ or "green“. Also, add
+// the private method "void cycleThroughPhases()“. Furthermore, there shall be
+// the private member _currentPhase which can take "red“ or "green“ as its
 // value.
 
-class TrafficLight {
+class TrafficLight : public TrafficObject {
  public:
   // constructor / desctructor
+  TrafficLight();
+  ~TrafficLight();
 
   // getters / setters
+  TrafficLightPhase getCurrentPhase();
 
   // typical behaviour methods
+
+  void waitForGreen();
+  void simulate();
 
  private:
   // typical behaviour methods
@@ -46,9 +54,10 @@ class TrafficLight {
   // TrafficLightPhase and use it within the infinite loop to push each new
   // TrafficLightPhase into it by calling send in conjunction with move
   // semantics.
-
+  void cycleThroughPhases();
   std::condition_variable _condition;
   std::mutex _mutex;
+  TrafficLightPhase _currentPhase;
 };
 
 #endif
